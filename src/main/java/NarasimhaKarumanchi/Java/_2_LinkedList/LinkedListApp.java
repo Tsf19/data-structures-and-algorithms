b4f4ab5fd5903fb3cd43434baa279749d9ff4e1c
Tousif <md.tousifmemon@gmail.com>
2019-05-25 03:32:03 +0530

    clean commit

--- src/main/java/NarasimhaKarumanchi/Java/_2_LinkedList/LinkedListApp.java
+++ src/main/java/NarasimhaKarumanchi/Java/_2_LinkedList/LinkedListApp.java
@@ -13,16 +13,11 @@ public class LinkedListApp {
 	public static void main(String[] args) {
 
 		LinkedListService list = new LinkedListServiceImplementatio();
-
 		list.insertAtEnd(10);
-		
 		list.insertAtEnd(20);
-		
 		list.insertAtEnd(30);
 		
-		list.insertAtBegining(40);
-		
-		list.getLengthOfLinkedList();
+		System.out.println(list.getLengthOfLinkedList());
 		System.out.println(list);
 		list.displayLinkedList();
 	}
