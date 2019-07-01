b5bde50ff38c6e6fac1892807197e5cec3969a37
md.tousif <DOMAIN\md.tousif@tousif-optiplex-3020.blr.cordis>
2019-07-01 17:46:55 +0530

    SimplePalindrome Added

---
+++ src/main/java/NarasimhaKarumanchi/Java/_3_Stacks/_4_StackProblemsAndSolutions/_4_SimplePalindrome.java
@@ -0,0 +1,23 @@
+package NarasimhaKarumanchi.Java._3_Stacks._4_StackProblemsAndSolutions;
+
+public class _4_SimplePalindrome {
+
+	public static boolean isPalindrome(String string) {
+		
+		int i,j;
+		for(i=0,j=string.length()-1; i<j && string.charAt(i) == string.charAt(j); i++, j--);
+		
+		if(i<j)
+			return false;
+		else
+			return true;
+		
+	}
+	
+	public static void main(String[] args) {
+
+		System.out.println(isPalindrome("12345iji54321"));
+		
+	}
+
+}
