900ed404ff3ab2ee1f343b7352e040e51b6824de
md.tousif <DOMAIN\md.tousif@tousif-optiplex-3020.blr.cordis>
2019-07-08 10:53:47 +0530

    convertCamelCaseToUnderscoreFormat Added

---
+++ src/main/java/Other/MyCodes/Logics/CamelCaseToUnderscoreFormat.java
@@ -0,0 +1,30 @@
+package Other.MyCodes.Logics;
+
+/**
+ * @author DOMAIN\md.tousif
+ *
+ */
+public class CamelCaseToUnderscoreFormat {
+
+	private static String convertCamelCaseToUnderscoreFormat(String text) {
+		
+		String result="";
+		
+		for (int i = 0; i < text.length(); i++) {
+			if(Character.isUpperCase(text.charAt(i))){    
+				String temp=""+text.charAt(i);
+				result=result+"_"+temp.toLowerCase();
+			}
+			else
+				result = result+text.charAt(i);
+		}
+		return result;
+	}
+	
+	public static void main(String[] args) {
+
+		System.out.println(convertCamelCaseToUnderscoreFormat("MohammedTousifShekhani"));
+		
+	}
+
+}
