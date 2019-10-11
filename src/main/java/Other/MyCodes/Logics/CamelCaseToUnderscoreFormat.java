58a65b6ad7a6b5cb3ea1092ed6b88464b9c21618
md.tousif <DOMAIN\md.tousif@tousif-optiplex-3020.blr.cordis>
2019-10-11 11:43:18 +0530

    Logics Added

--- src/main/java/Other/MyCodes/Logics/CamelCaseToUnderscoreFormat.java
+++ src/main/java/Other/MyCodes/Logics/CamelCaseToUnderscoreFormat.java
@@ -7,9 +7,9 @@ package Other.MyCodes.Logics;
 public class CamelCaseToUnderscoreFormat {
 
 	private static String convertCamelCaseToUnderscoreFormat(String text) {
-		
+
 		String result="";
-		
+
 		for (int i = 0; i < text.length(); i++) {
 			if(Character.isUpperCase(text.charAt(i))){    
 				String temp=""+text.charAt(i);
@@ -20,11 +20,18 @@ public class CamelCaseToUnderscoreFormat {
 		}
 		return result;
 	}
-	
+
 	public static void main(String[] args) {
 
 		System.out.println(convertCamelCaseToUnderscoreFormat("MohammedTousifShekhani"));
-		
+
+		String regex = "([a-z])([A-Z]+)";
+		String replacement = "$1_$2";
+		System.out.println("CustomLine"
+				.replaceAll(regex, replacement)
+				.toLowerCase());
+
+
 	}
 
 }
