3cf1dea57857c404ff9dddaba8d0da51e93d649a
Tousif <md.tousifmemon@gmail.com>
2023-07-18 20:19:55 +0530

    Updated: Test Strings in Java

--- src/main/java/Try.java
+++ src/main/java/Try.java
@@ -1,9 +1,45 @@
 
 public class Try {
+	
+	static void test1() {
+		
+		String s1 = "Hello";
+		String s2 = "World";
+		
+		String s3 = "Hello" + "World";
+		String s4 = "Hello" + "World";
+		
+		Boolean b1 = s3 == s4;
+		Boolean b2 = s3.equals(s4);
+		
+		System.out.println(b1);
+		System.out.println(b2);		
+		
+	}
+	
+	static void test2() {
+		
+		String s1 = "Hello";
+		String s2 = "World";
+		
+		String s3 = s1 + s2;
+		String s4 = s1 + s2;
+		
+		Boolean b1 = s3 == s4;
+		Boolean b2 = s3.equals(s4);
+		
+		System.out.println(b1);
+		System.out.println(b2);		
+		
+	}
+	
 	public static void main(String[] args) {
 		
-		String s1 = "1234567890"; //10 characters
-		System.out.println(s1.length()); //10
+		System.out.println("---------- calling test1()... ----------");
+		test1();
+
+		System.out.println("---------- calling test2()... ----------");
+		test2();
 		
 	}
 }
