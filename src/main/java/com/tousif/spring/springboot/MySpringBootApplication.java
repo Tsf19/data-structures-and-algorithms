d52caee339f509186367c9feeb27847aecd430ed
Tousif <md.tousifmemon@gmail.com>
2024-11-02 23:38:30 +0530

    Updated: MySpringBootApplication

--- src/main/java/com/tousif/spring/springboot/MySpringBootApplication.java
+++ src/main/java/com/tousif/spring/springboot/MySpringBootApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyApplication {
+public class MySpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyApplication.class, args);
+		SpringApplication.run(MySpringBootApplication.class, args);
 		System.out.println("WELCOME TO MY APPLICATION");
 	}
 }
